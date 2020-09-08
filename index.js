e7cde7c3448d9d7ba3010fbf53f460feaf15bbae
Jane Lane <loan.does@gmail.com>
2020-09-08 16:37:08 +0200

    maj index

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ console.log('Getting started with Node Slack SDK');
 
 const web = new WebClient(process.env.SLACK_TOKEN);
 
+var http = require('http');
+
+function requestListener(request, response) {
+	response.writeHead(200);
+	response.end('Coucou');
+
+	if (request.method === 'GET') {
+		console.log('requête reçue');
+	} else if (request.method === 'POST') {
+		console.log('no puedo');
+	}
+};
+
+var server = http.createServer(requestListener);
+
+server.listen(8080);
